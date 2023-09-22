e542c1a4489dabd067a0bb20bd1e96b7981dd938
Ahegys <andersonfelpe123@gmail.com>
2023-09-22 18:22:40 -0300

    [+]: setup basic instructions

--- src/config/init_lib.zig
+++ src/config/init_lib.zig
@@ -1,22 +1,31 @@
 const c = @cImport(@cInclude("SDL2/SDL.h"));
-const print = @import("std").debug.print;
-const comp = @import("../string_utis/equals.zig").comp;
+const std = @import("std");
+
+pub const CENTERED = c.STD_WINDOWPOS_CENTERED;
+pub const UNDEFINED = c.SDL_WINDOWPOS_UNDEFINED;
+
+pub const FULLSCREEN = c.SDL_WINDOW_FULLSCREEN;
+pub const FULLSCREEN_DESKTOP = c.SDL_WINDOW_FULLSCREEN_DESKTOP;
+pub const OPENGL = c.SDL_WINDOW_OPENGL;
+pub const VULKAN = c.SDL_WINDOW_VULKAN;
+pub const METAL = c.SDL_WINDOW_METAL;
+pub const HIDDEN = c.SDL_WINDOW_HIDDEN;
+pub const BORDERLESS = c.SDL_WINDOW_BORDERLESS;
+pub const RESIZABLE = c.SDL_WINDOW_RESIZABLE;
+pub const MINIMIZED = c.SDL_WINDOW_MINIMIZED;
+pub const MAXIMIZED = c.SDL_WINDOW_MAXIMIZED;
+pub const GRABBED = c.SDL_WINDOW_INPUT_GRABBED;
+pub const HIGHDPI = c.SDL_WINDOW_ALLOW_HIGHDPI;
+pub const DEFAULT = c.SDL_WINDOW_SHOWN;
+pub const ZiggError = c.SDL_GetError;
+pub var ctx: c_uint = DEFAULT;
 
 pub const init = struct {
-    var stateWin = "undefined";
-    pub fn setWindState(comptime state: ?[]const u8) i32 {
-        var setState: i32 = 0;
+    pub const setWinX: c_int = UNDEFINED;
+    pub const setWinY: c_int = UNDEFINED;
 
-        if (comp(state, "undefined")) {
-            setState = c.SDL_WINDOWPOS_UNDEFINED;
-        } else if (comp(state, "centered")) {
-            setState = c.SDL_WINDOWPOS_CENTERED;
-        }
-        return (setState);
-    }
-
-    pub fn init_display(width: i16, height: i16, comptime title: ?[]const u8) void {
-        const window = undefined;
+    pub fn init_display(width: i16, height: i16, comptime title: [*c]const u8) !void {
+        const print = std.debug.print;
 
         if (c.SDL_Init(c.SDL_INIT_VIDEO) < 0) {
             const sdlError = c.SDL_GetError();
@@ -25,11 +34,23 @@ pub const init = struct {
         }
         defer c.SDL_Quit();
 
-        window = c.SDL_CreateWindow(title, setWindState(), setWindState(), width, height, c.SDL_WINDOW_SHOWN);
+        const window = c.SDL_CreateWindow(title, setWinX, setWinY, width, height, ctx);
         if (window == null) {
-            const sdlError = c.SDL_GetError();
-            print("Error starting window: {s}\n", .{sdlError});
+            print("Error starting window: {s}\n", .{ZiggError()});
             return error.InitFailed;
         }
+
+        var exit: bool = false;
+        var event: c.SDL_Event = undefined;
+
+        while (!exit) {
+            while (c.SDL_PollEvent(&event) != 0) {
+                if (event.type == c.SDL_QUIT) {
+                    exit = true;
+                }
+            }
+        }
+
+        c.SDL_DestroyWindow(window);
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,7 @@
 //modified by Ahegys
 const std = @import("std");
-
+const start = @import("./config/init_lib.zig");
 pub fn main() !void {
     // Inicializar o SDL
-
-    const width = 800;
-    const height = 600;
-    // Aguardar por um evento de saída
-    var sair: bool = false;
-    var evento: c.SDL_Event = undefined;
-
-    while (!sair) {
-        while (c.SDL_PollEvent(&evento) != 0) {
-            if (evento.type == c.SDL_QUIT) {
-                sair = true;
-            }
-        }
-    }
-
-    // Destruir a janela e finalizar o SDL
-    c.SDL_DestroyWindow(window);
+    try start.init.init_display(320, 240, "window");
 }
