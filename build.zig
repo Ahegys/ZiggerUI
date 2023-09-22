dfbe6fd48ab5981b8ac6affff5517d0e20eab9a0
Ahegys <andersonfelpe123@gmail.com>
2023-09-22 12:15:33 -0300

    [+]: SDL starting

---
+++ build.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const Target = @import("std").Target;
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exeX86 = b.addExecutable(.{
+        .name = "Zigger_x86",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    exeX86.linkSystemLibraryName("SDL2");
+    exeX86.linkSystemLibraryName("c");
+    b.installArtifact(exeX86);
+}
---
+++ src/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const c = @cImport(@cInclude("SDL2/SDL.h"));
+
+pub fn main() !void {
+    // Inicializar o SDL
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) < 0) {
+        const sdlError = c.SDL_GetError();
+        std.debug.print("Erro ao inicializar o SDL: {s}\n", .{sdlError});
+        return error.InitFailed;
+    }
+
+    defer c.SDL_Quit();
+
+    const width = 800;
+    const height = 600;
+    const window = c.SDL_CreateWindow("Minha Janela SDL", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, width, height, c.SDL_WINDOW_SHOWN);
+    if (window == null) {
+        const sdlError = c.SDL_GetError();
+        std.debug.print("Erro ao criar a janela: {s}\n", .{sdlError});
+        return error.InitFailed;
+    }
+
+    // Aguardar por um evento de saída
+    var sair: bool = false;
+    var evento: c.SDL_Event = undefined;
+
+    while (!sair) {
+        while (c.SDL_PollEvent(&evento) != 0) {
+            if (evento.type == c.SDL_QUIT) {
+                sair = true;
+            }
+        }
+    }
+
+    // Destruir a janela e finalizar o SDL
+    c.SDL_DestroyWindow(window);
+}
