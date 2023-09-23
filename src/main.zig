5c5959a6cae416f6bd2a27e35ac8af78f0c2d527
Ahegys <andersonfelpe123@gmail.com>
2023-09-23 02:24:02 -0300

    [+]: add test for pixel and drawing in canvas

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,25 @@
 //modified by Ahegys
 const std = @import("std");
 const create = @import("./config/init_lib.zig");
+const set = @import("./config/pixel.zig");
+const c = @cImport(@cInclude("SDL2/SDL.h"));
 
-fn render() void {
-    std.debug.print("Hello World from {s} ", .{"render"});
+fn render(win: ?*c.SDL_Renderer) void {
+    const color = set.Color{
+        .r = 255,
+        .g = 122,
+        .b = 30,
+        .a = 255,
+    };
+    var i: i32 = 0;
+    while (i < 50) {
+        var j: i32 = 0;
+        while (j < 50) {
+            set.pixel(win, i, j, color);
+            j += 1;
+        }
+        i += 1;
+    }
 }
 
 fn setup() void {
@@ -11,5 +27,5 @@ fn setup() void {
 }
 
 pub fn main() !void {
-    try create.init.init_display(320, 240, "window", render, setup);
+    try create.init.display(320, 240, "window", render, setup);
 }
