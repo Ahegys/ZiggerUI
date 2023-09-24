b03b9efec84192fe8c3b7ef1a320f93a0ecd52d9
Ahegys <andersonfelpe123@gmail.com>
2023-09-24 01:02:14 -0300

    [+]: refactoring function initDisplay, create new set loops

--- src/config/init_lib.zig
+++ src/config/init_lib.zig
@@ -4,13 +4,14 @@ const props = @import("../layers/layer.zig");
 pub var ctx: c_uint = props.OPENGL;
 
 pub const init = struct {
-    pub fn display(width: i16, height: i16, comptime title: [*c]const u8, render: *const fn (?*props.c.SDL_Renderer) void, setup: *const fn () void) !void {
-        const print = std.debug.print;
-
+    pub fn display(width: i16, height: i16, comptime title: [*c]const u8) !void {
         try props.StartDisplay();
         try props.StartWindow(width, height, title, ctx);
         try props.render();
+    }
 
+    pub fn LoopHook(render: *const fn (?*props.c.SDL_Renderer) void, setup: *const fn () void) !void {
+        const print = std.debug.print;
         var exit: bool = false;
         var event: props.Event = undefined;
 
@@ -20,14 +21,16 @@ pub const init = struct {
                     exit = true;
                 }
             }
-            setup();
             const result = props.DrawColor(props.setRender, 0, 0, 0, 255);
             if (result < 0) {
                 print("Error to set renderer: {s}\n", .{props.ZiggError()});
             }
             try ClearRender(props.setRender);
+
+            setup();
             render(props.setRender);
         }
+        props.Loop(props.setRender);
         props.Destroy(props.window);
     }
 
